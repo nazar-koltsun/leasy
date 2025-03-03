80d5d63f8971b670e8e3a886ed2dc8fc6adf6b1f
Nazar Koltsun <nazar-koltsun@ukr.net>
2025-03-03 11:28:18 +0200

    Basic redesign of header to fit the main front page

--- components/Header.jsx
+++ components/Header.jsx
@@ -1,8 +1,13 @@
+'use client';
+
+import { usePathname } from 'next/navigation';
 import { cn } from '@/lib/utils';
 import Image from 'next/image';
 import Logo from '@/assets/logo.png';
 import SearchInput from '@/components/SearchInput';
 import Button from '@/components/Button';
+import HeaderNav from '@/components/HeaderNav';
+
 import BurgerIcon from '@/components/icons/BurgerIcon';
 import UserIcon from '@/components/icons/UserIcon';
 import LockIcon from '@/components/icons/LockIcon';
@@ -11,6 +16,11 @@ import NotificationIcon from '@/components/icons/NotificationIcon';
 import PlusCircleIcon from '@/components/icons/PlusCircleIcon';
 import UserPhoto from '@/assets/user-photo.png';
 
+const SHOW_DESKTOP_NAV_ROUTES = ['/'];
+
+// Thi is only for demo purpose, delete it when you will have the actual isAuth logic
+const SHOW_IS_AUTH_ROUTES = ['/', '/dashboard'];
+
 const DashboardLogo = () => (
   <div className="flex items-center gap-3">
     <GraphIcon />
@@ -18,8 +28,12 @@ const DashboardLogo = () => (
   </div>
 );
 
-const Header = ({ isDashboard = false, isAuth = false, className }) => {
+const Header = ({ isDashboard = false, className }) => {
   const flatBtnBaseClasses = 'w-8 h-8 flex items-center justify-center';
+
+  const pathname = usePathname();
+  const showNavOnDesktop = SHOW_DESKTOP_NAV_ROUTES.includes(pathname);
+  const isAuth = SHOW_IS_AUTH_ROUTES.includes(pathname);
 
   return (
     <header
@@ -33,24 +47,38 @@ const Header = ({ isDashboard = false, isAuth = false, className }) => {
         {isDashboard && <DashboardLogo />}
       </div>
 
-      <SearchInput
-        className="w-[450px] absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 order-2
-        max-1250:w-[300px] max-1100:w-full max-1100:mt-4 max-1100:relative max-1100:top-0 max-1100:left-0 
-        max-1100:translate-x-0 max-1100:translate-y-0 max-1100:order-last"
-        placeholder="Czego szukasz....?"
-        isSecondary
-      />
+      <HeaderNav className={cn('hidden', showNavOnDesktop && 'block')} />
 
-      <div className={cn("flex gap-3 items-center order-3 max-700:mt-4 max-620:gap-3", isAuth && 'gap-5')}>
-        {isAuth && (
-          <Button variant='secondary' className='font-firaSans font-medium'>
+      {!showNavOnDesktop && (
+        <SearchInput
+          className="w-[450px] absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 order-2
+            max-1250:w-[300px] max-1100:w-full max-1100:mt-4 max-1100:relative max-1100:top-0 max-1100:left-0 
+            max-1100:translate-x-0 max-1100:translate-y-0 max-1100:order-last"
+          placeholder="Czego szukasz....?"
+          isSecondary
+        />
+      )}
+
+      <div
+        className={cn(
+          'flex gap-3 items-center order-3 max-700:mt-4 max-620:gap-3',
+          isAuth && 'gap-5'
+        )}
+      >
+        {isDashboard && (
+          <Button variant="secondary" className="font-firaSans font-medium">
             <PlusCircleIcon /> Dodaj ogłoszenie
           </Button>
         )}
 
         <Button
           variant="flat"
-          className={cn("group transition duration-300 ease-in-out", flatBtnBaseClasses, !isAuth && 'mr-2')}
+          className={cn(
+            'group transition duration-300 ease-in-out',
+            flatBtnBaseClasses,
+            !isAuth && 'mr-2',
+            showNavOnDesktop && 'hidden'
+          )}
         >
           <BurgerIcon className="group-hover:fill-[#0d7a50]" />
         </Button>
@@ -73,21 +101,34 @@ const Header = ({ isDashboard = false, isAuth = false, className }) => {
           </>
         )}
 
+        {isDashboard && (
+          <Button
+            variant="flat"
+            className={cn('group relative', flatBtnBaseClasses)}
+          >
+            <NotificationIcon className="group-hover:fill-fireOpal transition duration-300 ease-in-out" />
+            <span className="absolute right-0 top-0 w-[7px] h-[7px] bg-fireOpal rounded-full"></span>
+          </Button>
+        )}
+
         {isAuth && (
           <>
-            <Button variant='flat' className={cn('group relative', flatBtnBaseClasses)}>
-              <NotificationIcon className="group-hover:fill-fireOpal transition duration-300 ease-in-out" />
-              <span className='absolute right-0 top-0 w-[7px] h-[7px] bg-fireOpal rounded-full'></span>
-            </Button>
+            <Button
+              className="group relative w-12 h-12 p-0 rounded-full bg-transparent border border-[#ddd] 
+            hover:bg-transparent shadow-doobleSmallShadow"
+            >
+              <Image
+                src={UserPhoto}
+                alt="User photo"
+                className="absolute left-1/2 top-1/2 
+              -translate-x-1/2 -translate-y-1/2 w-10 h-10"
+              />
 
-            <Button className='group relative w-12 h-12 p-0 rounded-full bg-transparent border border-[#ddd] 
-            hover:bg-transparent shadow-doobleSmallShadow'>
-              <Image src={UserPhoto} alt='User photo' className='absolute left-1/2 top-1/2 
-              -translate-x-1/2 -translate-y-1/2 w-10 h-10' />
-
-              <div role='presentation' 
-                className='w-full h-full border-[3px] border-green rounded-full 
-                transition duration-300 ease-in-out group-hover:border-[#0d7a50]'></div>
+              <div
+                role="presentation"
+                className="w-full h-full border-[3px] border-green rounded-full 
+                transition duration-300 ease-in-out group-hover:border-[#0d7a50]"
+              ></div>
             </Button>
           </>
         )}
---
+++ components/HeaderNav.jsx
@@ -0,0 +1,7 @@
+import { cn } from "@/lib/utils";
+
+const HeaderNav = ({ className }) => {
+  return <nav className={cn('order-2', className)}>nav</nav>;
+};
+
+export default HeaderNav;
